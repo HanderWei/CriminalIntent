8d36547477a4628aaa92c4d1bcd165dd34516c18
Hander <hander_wei@163.com>
2016-11-20 22:44:17 +0800

    @dev 基本UI搭建

---
+++ app/src/main/java/me/chen_wei/criminalintent/Crime.java
@@ -0,0 +1,51 @@
+package me.chen_wei.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Hander on 16/11/20.
+ * <p>
+ * Email : hander_wei@163.com
+ */
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
